b71089a61b66f8294e9b03631790a3ccf21e66f8
Mark <gardiolamark2003@gmail.com>
2026-03-07 10:49:21 +0800

    navbar and innovations page, not yet mobile responsive

---
+++ pages/innovations.php
@@ -0,0 +1,187 @@
+<?php
+include '../includes/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Innovations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/isc-website/assets/css/innovations.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col-4 me-5 text-light d-flex align-items-center justify-content-center rounded-end-4" style="height: 66px;">
+                <h1 class="stories m-0">Stories and Innovations</h1>
+            </div>
+            <div class="col bg-transparent d-flex align-items-center justify-content-start border border-end-0 border-dark rounded-start-4" style="height: 66px;">
+                <p class="feature m-0 ps-2">Featuring stories of ISC members innovations, events, and alumni insights from the industries.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="row g-4">
+
+            <!-- Card -->
+            <div class="col-lg-3 col-md-6">
+                <div class="innovation-card">
+                    <img src="/isc-website/assets/images/sample.jpg" class="img-fluid card-img-top" alt="AI Image">
+
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            How Monk's Hill Ventures Built AI Fluency Across Four Countries
+                        </h5>
+
+                        <p class="author">by Bill Gates</p>
+
+                        <p class="card-text">
+                            When Monk's Hill Ventures gathered its team across four countries
+                            for an AI workshop, they expected tips on prompts and productivity.
+                            What they got instead was a hands-on, curiosity-driven lesson
+                            in how to make AI work for real-world tasks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="innovation-card">
+                    <img src="/isc-website/assets/images/sample.jpg" class="img-fluid card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            How Monk's Hill Ventures Built AI Fluency Across Four Countries
+                        </h5>
+                        <p class="author">by Bill Gates</p>
+                        <p class="card-text">
+                            When Monk's Hill Ventures gathered its team across four countries
+                            for an AI workshop, they expected tips on prompts and productivity.
+                            What they got instead was a hands-on, curiosity-driven lesson
+                            in how to make AI work for real-world tasks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="innovation-card">
+                    <img src="/isc-website/assets/images/sample.jpg" class="img-fluid card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            How Monk's Hill Ventures Built AI Fluency Across Four Countries
+                        </h5>
+                        <p class="author">by Bill Gates</p>
+                        <p class="card-text">
+                            When Monk's Hill Ventures gathered its team across four countries
+                            for an AI workshop, they expected tips on prompts and productivity.
+                            What they got instead was a hands-on, curiosity-driven lesson
+                            in how to make AI work for real-world tasks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="innovation-card">
+                    <img src="/isc-website/assets/images/sample.jpg" class="img-fluid card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            How Monk's Hill Ventures Built AI Fluency Across Four Countries
+                        </h5>
+                        <p class="author">by Bill Gates</p>
+                        <p class="card-text">
+                            When Monk's Hill Ventures gathered its team across four countries
+                            for an AI workshop, they expected tips on prompts and productivity.
+                            What they got instead was a hands-on, curiosity-driven lesson
+                            in how to make AI work for real-world tasks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="innovation-card">
+                    <img src="/isc-website/assets/images/sample.jpg" class="img-fluid card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            How Monk's Hill Ventures Built AI Fluency Across Four Countries
+                        </h5>
+                        <p class="author">by Bill Gates</p>
+                        <p class="card-text">
+                            When Monk's Hill Ventures gathered its team across four countries
+                            for an AI workshop, they expected tips on prompts and productivity.
+                            What they got instead was a hands-on, curiosity-driven lesson
+                            in how to make AI work for real-world tasks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="innovation-card">
+                    <img src="/isc-website/assets/images/sample.jpg" class="img-fluid card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            How Monk's Hill Ventures Built AI Fluency Across Four Countries
+                        </h5>
+                        <p class="author">by Bill Gates</p>
+                        <p class="card-text">
+                            When Monk's Hill Ventures gathered its team across four countries
+                            for an AI workshop, they expected tips on prompts and productivity.
+                            What they got instead was a hands-on, curiosity-driven lesson
+                            in how to make AI work for real-world tasks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="innovation-card">
+                    <img src="/isc-website/assets/images/sample.jpg" class="img-fluid card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            How Monk's Hill Ventures Built AI Fluency Across Four Countries
+                        </h5>
+                        <p class="author">by Bill Gates</p>
+                        <p class="card-text">
+                            When Monk's Hill Ventures gathered its team across four countries
+                            for an AI workshop, they expected tips on prompts and productivity.
+                            What they got instead was a hands-on, curiosity-driven lesson
+                            in how to make AI work for real-world tasks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="innovation-card">
+                    <img src="/isc-website/assets/images/sample.jpg" class="img-fluid card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            How Monk's Hill Ventures Built AI Fluency Across Four Countries
+                        </h5>
+                        <p class="author">by Bill Gates</p>
+                        <p class="card-text">
+                            When Monk's Hill Ventures gathered its team across four countries
+                            for an AI workshop, they expected tips on prompts and productivity.
+                            What they got instead was a hands-on, curiosity-driven lesson
+                            in how to make AI work for real-world tasks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
